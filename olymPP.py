b6281cc68604691f8695befe49f05064b7f62c3b
TheXDShrimp <thexdshrimp@LAPTOP-POD2MG9N>
2023-11-20 13:01:21 -0500

    move command added

--- olymPP.py
+++ olymPP.py
@@ -1,7 +1,7 @@
 import olympe
 import os
 import time
-from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveBy
 
 DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
 
@@ -13,7 +13,23 @@ def test_takeoff():
     time.sleep(10)
     assert drone(Landing()).wait().success()
     drone.disconnect()
+    
+def move_forward():
+    distance = 5
+    duration = 2
+
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    # Move the drone forward
+    assert drone(TakeOff()).wait().success()
+    assert drone(moveBy(distance, 0, 0, 0)).wait().success()
+    time.sleep(duration)
+    assert drone(Landing()).wait().success()
+    drone.disconnect()
+
 
 
 if __name__ == "__main__":
     test_takeoff()
+
+    move_forward()
